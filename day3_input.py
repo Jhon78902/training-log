d3776b915d5eeefd14cebcdba784ecdda81df889
Jhon78902 <mdsuzanmia78902@gmail.com>
2025-12-04 11:08:36 +0900

    Add day3_input.py(input and type connversion)

---
+++ day3_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+print("Hello,",name)
+age = input("How old are you?")
+print("Age,",age)
+TOEIC_Score = float(input("What is your TOEIC score?"))
+print("TOEIC Score:",TOEIC_Score)
+ready_iput = input("Are you ready to freelace? (yes/no):")
+I_am_ready_to_freelance = True if ready_iput.lower()== "yes" else False
+print("Ready to freelance?:",I_am_ready_to_freelance)
