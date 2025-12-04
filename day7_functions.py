fcba124c9780edca1ac225d501c5aadbf54b3fd8
Jhon78902 <mdsuzanmia78902@gmail.com>
2025-12-04 14:00:39 +0900

    Add day7_functions.py(functions practice)

---
+++ day7_functions.py
@@ -0,0 +1,10 @@
+def introduce():
+    print("My name is Jhon")
+    print("I study IT (情報処理学科)")
+    print("My carrer goal is remote freelancing by 2026")
+
+def add_number (a, b):
+    return a + b
+introduce()
+result = add_number(9, 6)
+print("Sum is:", result)
