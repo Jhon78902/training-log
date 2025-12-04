0d7318dadbec730a28c379ffad84e1691e645356
Jhon78902 <mdsuzanmia78902@gmail.com>
2025-12-04 10:33:44 +0900

    Upload Day 2 script with name, age, TOEIC score, and readiness flag.

---
+++ day2_variables.py
@@ -0,0 +1,8 @@
+Name = "Jhon"
+Age  = 28
+TOEIC_Score = 725
+I_am_ready_to_freelance_now = False
+print("Name:",Name)
+print("Age:",Age)
+print("TOIC Score:", TOEIC_Score)
+print("Ready to freelance:", I_am_ready_to_freelance_now)
