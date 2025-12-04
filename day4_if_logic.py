96c47075acdde8ee583588e2c5c733a6ffa918d9
Jhon78902 <mdsuzanmia78902@gmail.com>
2025-12-04 12:14:56 +0900

    Add day4_float.py (practice with int and float)

---
+++ day4_if_logic.py
@@ -0,0 +1,24 @@
+age = 18
+age = int(input("How old are you?"))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a child.")
+TOEIC_Score = 600
+TOEIC_Score = int(input("What is your TOEIC score?"))
+if TOEIC_Score >= 600:
+    print("Your English lvel is good.")
+else:
+    print("You need more practice.")
+# Integers (Whole Number)
+age = 27
+TOEIC_Score = 725
+# Floats (decimal numbers)
+height = 1.75 # meter
+weight = 68.5 # weight
+print("Age:", age)
+print("TOEIC Score", TOEIC_Score)
+print("Height (m):", height)
+print("Wight (kg):", weight)
+bmi = weight/(height **2) # BMI formula
+print("Your BMI is:", bmi)
