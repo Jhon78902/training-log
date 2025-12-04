4485c105f16b00071b72997ac4a09dd55c45aafd
Jhon78902 <mdsuzanmia78902@gmail.com>
2025-12-04 13:14:21 +0900

    Add day6_lists_tupls_(lists and tuples)

---
+++ day6_lists_tuples.py
@@ -0,0 +1,8 @@
+fruits = ["Watermelon", "Guava", "Jackfruit"]
+print(fruits[0]) # Watermelon
+fruits[1] = "Mango"
+print(fruits)   #['Watermelon', 'Mango','cherry']
+country = ("Bangladesh", "America", "England")
+print(country[0])   #Bangladesh
+country[1]= "India"
+
